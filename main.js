867fd577f901f0dfc41e536dcabb22e7e0a054d6
Saikiran Rudra <saikiranrudra2@gmail.com>
2020-01-01 15:50:51 +0530

    cross origin fix added

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+//fix cross origin problem
+//use this middleware in express server
+//fix start
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+// fix end
+
 // AXIOS GLOBALS
 axios.defaults.headers.common['X-Auth-Token'] =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
